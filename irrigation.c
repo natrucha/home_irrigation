65f398108cefdf97178e035bd6db3aeb7f7a4db4
Natalie Pueyo <natapueyo@gmail.com>
2024-07-19 16:46:26 -0700

    refactoring parsing CIMIS JSON output

--- irrigation.c
+++ irrigation.c
@@ -42,7 +42,7 @@ struct write_result {
 };
 
 static size_t write_response(void *ptr, size_t size, size_t nmemb, void *stream) {
-    /* Save the GET response from into write_result struct, function from jannson exmaple code */
+    /* Save the GET response from into write_result struct, function from jannson example code */
     struct write_result *result = (struct write_result *)stream;
 
     if (result->pos + size * nmemb >= BUFFER_SIZE - 1) {
@@ -132,13 +132,111 @@ error:
 }
 
 
+typedef struct cimis_results {
+    float Et0;
+    float precip;
+    int parse_errors;   // will save how many json types were incorrect
+} cimis_results;
+
+
+cimis_results parseCimisJson(json_t *json_root){
+    // to check what type it actually is, go to https://jansson.readthedocs.io/en/2.8/apiref.html#c.json_type
+    // and check typeof (it's an int and the types are listed in order)
+
+    // obtain the Eto values for each day requested to CIMIS
+    json_t *Data, *Providers, *get_records, *Records;
+    const char *eto_value, *precip_value; // values adding up the total precipitation and ETo over specified range
+    cimis_results cimis_out;
+
+    int error_count = 0;
+    float total_precipitation = 0.0; 
+    float total_eto = 0.0;
+
+    Data = json_object_get(json_root, "Data");
+    // Data has to be an object
+    if (!json_is_object(Data)) {
+        // fprintf(stderr, "error: Data is not an object\n");
+        // printf("    Data is a(n) %d\n", json_typeof(json_root));
+        error_count++;
+    } 
+    Providers = json_object_get(Data, "Providers");
+    if (!json_is_array(Providers)) {
+        // fprintf(stderr, "error: Providers is not an array\n");
+        // printf("    is Providers is an array? (1==yes) %d\n", json_is_array(Providers));
+        error_count++; 
+    } 
+    get_records = json_array_get(Providers, 0);
+    if (!json_is_object(get_records)) {
+        error_count++;  
+    } 
+    Records = json_object_get(get_records, "Records");
+    if (!json_is_array(Records)) {
+        error_count++; 
+    }
+    // else {
+    //     printf("The number of days being analyzed is %lu days\n", json_array_size(Records));
+    // }
+
+    for (int i = 0; i < json_array_size(Records); i++) {
+        json_t *get_daydata, *DayAsceEto, *EToValue, *DayPrecip, *PrecipValue;
+
+        get_daydata = json_array_get(Records, i);
+        if (!json_is_object(get_daydata)) {
+            error_count++;  
+        } 
+        DayAsceEto = json_object_get(get_daydata, "DayAsceEto");
+        if (!json_is_object(DayAsceEto)) {
+            error_count++; 
+        } 
+        EToValue = json_object_get(DayAsceEto, "Value");
+        if (!json_is_string(EToValue)) {
+            error_count++; 
+        } 
+
+        eto_value = json_string_value(EToValue);
+            // printf("%.8s %.*s\n", json_string_value(Providers), newline_offset(eto_value),
+            //     eto_value);
+
+        total_eto = total_eto + strtof(eto_value, NULL);
+
+        // *************** get the Precipitation data for the day *************** //
+        DayPrecip = json_object_get(get_daydata, "DayPrecip");
+        if (!json_is_object(DayPrecip)) {
+            error_count++; 
+        }
+
+        PrecipValue = json_object_get(DayPrecip, "Value");
+        if (json_is_null(PrecipValue)) { 
+            // precipitation could be null, just set as zero for now (likely that they don't have the data for the whole day so they provide null)
+            // precip_value = json_string_value(PrecipValue);
+            printf("Precipitation value for the day is null (probably the last day if only one message appears!)\n");
+        } else if (!json_is_string(PrecipValue)) {
+            // fprintf(stderr, "error: PrecipValue is not a string\n");
+            // printf("    PrecipValue is a(n) %d\n", json_typeof(PrecipValue));
+            error_count++; 
+        } else {
+            precip_value = json_string_value(PrecipValue);
+            // printf("%.8s %.*s\n", json_string_value(Providers), newline_offset(precip_value),
+            // precip_value);
+
+            total_precipitation = total_precipitation + strtof(precip_value, NULL);
+        }
+   
+    }
+
+    cimis_out.parse_errors = error_count;
+    cimis_out.Et0 = total_eto;
+    cimis_out.precip = total_precipitation;
+
+    return cimis_out;
+}
+
+
 
 int main(){
     char *cimis_station = CIMIS_STATION;
     char *cimis_app_key = APP_KEY;
     // printf("Example of using CMake input file, \n     Irrigation Major Version %d \n     Irrigation Minor Version %d \n", Irrigation_VERSION_MAJOR, Irrigation_VERSION_MINOR);
-
-    const char *eto_value, *precip_value; // values adding up the total precipitation and ETo over specified range
 
     time_t date_today, date_start;
     char today_buffer[80], start_buffer[80];
@@ -146,12 +244,6 @@ int main(){
     struct tm tm_out_today, tm_out_start;
 
     int num_days = 7; // how many days of data do we want to use?
-
-    // obtain the Eto values for each day requested to CIMIS
-    json_t *Data, *Providers, *get_records, *Records;
-
-    float total_precipitation = 0.0; 
-    float total_eto = 0.0;
 
     size_t i;
     char *text;
@@ -163,28 +255,30 @@ int main(){
     json_t *root_last;
     json_error_t error_last;
 
-    // provides the current date and time in seconds since the Epoch for the end date provided to CIMIS
-    time(&date_today);
-    // subtract the number of days (in seconds) of desired CIMIS data to obtain a start date
-    date_start = date_today - num_days*(24*60*60);
+    // // provides the current date and time in seconds since the Epoch for the end date provided to CIMIS
+    // time(&date_today);
+    // // subtract the number of days (in seconds) of desired CIMIS data to obtain a start date
+    // date_start = date_today - num_days*(24*60*60);
 
-    // represent the start and end dates in date and time components (local time)
-    localtime_r(&date_today, &tm_out_today);
-    localtime_r(&date_start, &tm_out_start);
+    // // represent the start and end dates in date and time components (local time)
+    // localtime_r(&date_today, &tm_out_today);
+    // localtime_r(&date_start, &tm_out_start);
 
-    // format the strings according to the format specified by CIMIS 
-    strftime(today_buffer, sizeof(today_buffer), "%Y-%m-%d", &tm_out_today);
-    strftime(start_buffer, sizeof(start_buffer), "%Y-%m-%d", &tm_out_start);
+    // // format the strings according to the format specified by CIMIS 
+    // strftime(today_buffer, sizeof(today_buffer), "%Y-%m-%d", &tm_out_today);
+    // strftime(start_buffer, sizeof(start_buffer), "%Y-%m-%d", &tm_out_start);
 
-    // stitch together the file name strings
-    char *file_name = malloc(strlen("cimis_") + strlen(start_buffer) + strlen("_") + strlen(today_buffer) + strlen(".json") + 1); // +1 for the null-terminator
-    // make function that has full_url passed as a pointer ref to concatenate the values internally, but can still free the pointer outside of the function
-    // add checks for errors in malloc here
-    strcpy(file_name, "cimis_");
-    strcat(file_name, start_buffer);
-    strcat(file_name, "_");
-    strcat(file_name, today_buffer);
-    strcat(file_name, ".json");
+    // // stitch together the file name strings
+    // char *file_name = malloc(strlen("cimis_") + strlen(start_buffer) + strlen("_") + strlen(today_buffer) + strlen(".json") + 1); // +1 for the null-terminator
+    // // make function that has full_url passed as a pointer ref to concatenate the values internally, but can still free the pointer outside of the function
+    // // add checks for errors in malloc here
+    // strcpy(file_name, "cimis_");
+    // strcat(file_name, start_buffer);
+    // strcat(file_name, "_");
+    // strcat(file_name, today_buffer);
+    // strcat(file_name, ".json");
+
+    char *file_name = "cimis_2023-07-06_2023-07-13.json";
     printf("Testing to see if file %s exists\n", file_name);
 
     // check if call has already been made for this dataset by checking if a file already exists for it
@@ -235,164 +329,50 @@ int main(){
         root = json_load_file(file_name, 0, &error);
         if(!root) {
             printf("Could not open file %s\n", file_name);
-            fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
+            fprintf(stderr, "ERROR: on line %d: %s\n", error.line, error.text);
             return 1;
         }
         // clean up by closing the file
         fclose(open_file);
     }
-    
-    Data = json_object_get(root, "Data");
-    if (!json_is_object(Data)) {
-        fprintf(stderr, "error: Data is not an object\n");
-        // to check what type it actually is, go to https://jansson.readthedocs.io/en/2.8/apiref.html#c.json_type
-        // and check typeof (it's an int and the types are listed in order)
-        printf("    Data is a(n) %d\n", json_typeof(root));
-        json_decref(root);
-        return 1;
-    } 
-    // else {
-    //     printf("Data correctly identified\n");
-    // }
-    
-    Providers = json_object_get(Data, "Providers");
-    if (!json_is_array(Providers)) {
-        fprintf(stderr, "error: Providers is not an array\n");
-        printf("    is Providers is an array? (1==yes) %d\n", json_is_array(Providers));
-        printf("    otherwise, Providers is a(n) %d\n", json_typeof(Providers));
-        json_decref(root);
-        return 1; 
-    } 
-    // else {
-    //     printf("Providers correctly identified\n");
-    // }
 
-    get_records = json_array_get(Providers, 0);
-    if (!json_is_object(get_records)) {
-        fprintf(stderr, "error: get_records is not an object\n");
-        printf("    is get_records an array? (1==yes) %d\n", json_is_array(get_records));
-        printf("    otherwise, get_records is a(n) %d\n", json_typeof(get_records));
-        json_decref(root);
-        return 1; 
-    } 
-    // else {
-    //     printf("get_records correctly identified\n");
-    // }
-
-    Records = json_object_get(get_records, "Records");
-    if (!json_is_array(Records)) {
-        fprintf(stderr, "error: Records is not an array\n");
-        printf("    Records is a(n) %d\n", json_typeof(Records));
+    cimis_results cimis_out;
+    cimis_out = parseCimisJson(root);
+    if (cimis_out.parse_errors > 0){
+        fprintf(stderr, "ERROR: there were %d type errors when parsing the Et0 JSON file.\n", cimis_out.parse_errors);
         json_decref(root);
         return 1;
     }
-    else {
-        printf("The number of days being analyzed is %lu days\n", json_array_size(Records));
-    }
 
-    for (int i = 0; i < json_array_size(Records); i++) {
-        json_t *get_daydata, *DayAsceEto, *EToValue, *DayPrecip, *PrecipValue;
+    printf("CIMIS Et0 reads %.2f\n", cimis_out.Et0);
+    printf("CIMIS precip reads %.2f\n", cimis_out.precip);
 
+        
+    // printf("\n\nThe total eto is             %f\n", total_eto);
+    // printf("The total precipitation is   %f (inches of rain)\n\n", total_precipitation);
 
-        // Records is an array with an Eto value for each day requested in the url
-        get_daydata = json_array_get(Records, i);
-        if (!json_is_object(get_daydata)) {
-            fprintf(stderr, "error: get_daydata is not an object\n");
-            printf("    is get_daydata an array? (1==yes) %d\n", json_is_array(get_daydata));
-            printf("    otherwise, get_daydata is a(n) %d\n", json_typeof(get_daydata));
-            json_decref(root);
-            return 1; 
-        } 
-        // else {
-        //     printf("get_daydata correctly identified\n");
-        // }
+    // // read last irrigation file
+    // char *last_file = "last_irrigation.json";
+    // FILE *open_last_file = fopen(last_file, "r");
 
-        // get the ETo data for the day
-        DayAsceEto = json_object_get(get_daydata, "DayAsceEto");
-        if (!json_is_object(DayAsceEto)) {
-            fprintf(stderr, "error: DayAsceEto is not an object\n");
-            printf("    DayAsceEto is a(n) %d\n", json_typeof(DayAsceEto));
-            json_decref(root);
-            return 1;
-        } 
-        // else {
-        //     printf("DayAsceEto correctly identified\n");
-        // }
-
-        EToValue = json_object_get(DayAsceEto, "Value");
-        if (!json_is_string(EToValue)) {
-            fprintf(stderr, "error: EToValue is not a string\n");
-            printf("    EToValue is a(n) %d\n", json_typeof(EToValue));
-            json_decref(root);
-            return 1;
-        } 
-        // else {
-        //     printf("ETo value for the day is a string equal to:\n");
-        // }
-
-        eto_value = json_string_value(EToValue);
-            // printf("%.8s %.*s\n", json_string_value(Providers), newline_offset(eto_value),
-            //     eto_value);
-
-        total_eto = total_eto + strtof(eto_value, NULL);
-
-        // get the Precipitation data for the day
-        DayPrecip = json_object_get(get_daydata, "DayPrecip");
-        if (!json_is_object(DayPrecip)) {
-            fprintf(stderr, "error: DayPrecip is not an object\n");
-            printf("    DayPrecip is a(n) %d\n", json_typeof(DayPrecip));
-            json_decref(root);
-            return 1;
-        } //else {
-        //     printf("DayPrecip correctly identified\n");
-        // }
-
-        PrecipValue = json_object_get(DayPrecip, "Value");
-        if (json_is_null(PrecipValue)) { 
-            // precipitation could be null, just set as zero for now (likely that they don't have the data for the whole day so they provide null)
-            // precip_value = json_string_value(PrecipValue);
-            printf("Precipitation value for the day is null (probably the last day if only one message appears!)\n");
-        } else if (!json_is_string(PrecipValue)) {
-            fprintf(stderr, "error: PrecipValue is not a string\n");
-            printf("    PrecipValue is a(n) %d\n", json_typeof(PrecipValue));
-            json_decref(root);
-            return 1;
-        } else {
-            // printf("DayPrecip value for the day is a string equal to:\n");
-            precip_value = json_string_value(PrecipValue);
-            // printf("%.8s %.*s\n", json_string_value(Providers), newline_offset(precip_value),
-            // precip_value);
-
-            total_precipitation = total_precipitation + strtof(precip_value, NULL);
-        }
-   
-    }
-    
-    printf("\n\nThe total eto is             %f\n", total_eto);
-    printf("The total precipitation is   %f (inches of rain)\n\n", total_precipitation);
-
-    // read last irrigation file
-    char *last_file = "last_irrigation.json";
-    FILE *open_last_file = fopen(last_file, "r");
-
-    printf("Opening Last Irrigation JSON file\n");
-    root_last = json_load_file(last_file, 0, &error_last);
-    if(!root_last) {
-        printf("Could not open file %s\n", last_file);
-        fprintf(stderr, "error: on line %d: %s\n", error_last.line, error_last.text);
-        return 1;
-    }
-    // clean up by closing the file
-    fclose(open_last_file); 
+    // printf("Opening Last Irrigation JSON file\n");
+    // root_last = json_load_file(last_file, 0, &error_last);
+    // if(!root_last) {
+    //     printf("Could not open file %s\n", last_file);
+    //     fprintf(stderr, "error: on line %d: %s\n", error_last.line, error_last.text);
+    //     return 1;
+    // }
+    // // clean up by closing the file
+    // fclose(open_last_file); 
 
     float veggie, backyard, inside, berry, front, grape;
 
-    float effective_precipitation = total_precipitation * 0.5;
+    float effective_precipitation = cimis_out.precip * 0.5;
 
 
     // calculations for each garden location:
     //       Veggie patch:     PF = 1.0, LA = 96 sq. ft
-    veggie = (total_eto * 1.0 * 96.0) - effective_precipitation;
+    veggie = (cimis_out.Et0 * 1.0 * 96.0) - effective_precipitation;
     if (veggie <= 0.0) { 
         veggie = 0.0; 
         printf("veggie should be watered!\n");
@@ -401,7 +381,7 @@ int main(){
     }
 
     //       Backyard:         PF = 0.5, LA = 300? sq. ft
-    backyard = (total_eto * 0.5 * 300.0) - effective_precipitation;
+    backyard = (cimis_out.Et0 * 0.5 * 300.0) - effective_precipitation;
     if (backyard <= 0.0) { 
         backyard = 0.0; 
         printf("backyard should be watered!\n");
@@ -410,7 +390,7 @@ int main(){
     }
 
     //       Inside Side Yard: PF = 0.5?, LA = 64? sq. ft
-    inside = (total_eto * 0.5 * 64.0) - effective_precipitation;
+    inside = (cimis_out.Et0 * 0.5 * 64.0) - effective_precipitation;
     if (inside <= 0.0) { 
         inside = 0.0; 
         printf("inside should be watered!\n");
@@ -419,7 +399,7 @@ int main(){
     }
 
     //       Berry Patch:      PF = 0.8?, LA = 64? sq. ft
-    berry = (total_eto * 0.8 * 64.0) - effective_precipitation;
+    berry = (cimis_out.Et0 * 0.8 * 64.0) - effective_precipitation;
     if (berry <= 0.0) { 
         berry = 0.0;
         printf("berry should be watered!\n");
@@ -428,7 +408,7 @@ int main(){
     }
 
     //       Native Front:     PF = 0.3?, LA = 32? sq. ft
-    front = (total_eto * 0.3 * 32.0) - effective_precipitation;
+    front = (cimis_out.Et0 * 0.3 * 32.0) - effective_precipitation;
     if (front <= 0.0) { 
         front = 0.0; 
         printf("front should be watered!\n");
@@ -437,7 +417,7 @@ int main(){
     }
 
     //       Grape:            PF = 0.5?, LA = 8 sq. ft
-    grape = (total_eto * 0.5 * 8.0) - effective_precipitation;
+    grape = (cimis_out.Et0 * 0.5 * 8.0) - effective_precipitation;
     if (grape <= 0.0) { 
         grape = 0.0; 
         printf("grape should be watered!\n");
